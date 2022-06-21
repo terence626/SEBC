b6493e47174940c9d945af212aeda4cea1a9bc7c
Terence Way <terence_way@hotmail.com>
2022-06-21 16:48:25 -0700

    SEBC As4 initial commit

---
+++ As4/QCC-SE-BC_As4_Way-Terence.js
@@ -0,0 +1,58 @@
+
+// Part A: BMI
+//BMI = WEIGHT / (HEIGHT*HEIGHT)
+
+document.write("QCC SEBC Assignment 4 by Terence Way<BR>");
+document.write("Part A - BMI:<BR><BR><BR>");
+
+let peterBMI = 92/(1.95*1.95);
+let lucasBMI = 78/(1.69*1.69);
+if(lucasBMI >= peterBMI)
+  {
+    document.write("Lucas's BMI of ("+ lucasBMI + ") is higher than Peter's BMI of ("+ peterBMI + ").");
+  }
+  else if(peterBMI > lucasBMI){
+    document.write("Peter's BMI of ("+ peterBMI + ") is higher than Lucas' BMI of ("+ lucasBMI + ").");
+    }
+
+
+
+// Part B: The temperature converter
+
+let celsTemp = 37;
+
+let fahrTemp = 108;
+
+function tempToFa (deg){
+ return (deg *9/5)+32;
+
+}
+function tempToCel(deg){
+ return (deg-32)*5/9;
+}
+
+let celToFa = tempToFa(celsTemp);
+
+alert(celsTemp + ' C is '+ celToFa +" F.");
+
+let faToCel = tempToCel(fahrTemp);
+alert( fahrTemp + " F is "+ faToCel + " C.");
+
+
+//
+//
+// //Part C: prompt()
+//
+// user = prompt("Enter a number: ");
+// if(user == ""){
+//   console.log("Please enter a number!");
+// } else if( user == 10){
+//   console.log("You win 10 points.");
+// }else if(user == 8 ){
+//   console.log("You win 8 points.");
+// }else{
+//   console.log("NOT MATCHED!!");
+// }
+//
+//
+//
